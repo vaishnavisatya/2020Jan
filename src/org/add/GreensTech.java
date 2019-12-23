ce177141482a41d05becb80d59450d0a3a35116b
sandeepvalmeti <sandeepvalmeti@gmail.com>
2019-12-23 19:35:13 +0530

    user1

--- src/org/add/GreensTech.java
+++ src/org/add/GreensTech.java
@@ -2,6 +2,9 @@ package org.add;
 
 public class GreensTech {
 	public void GreensOmr(String GreenOmr) {
+		System.out.println("satya");
+		System.out.println("dec");
+		System.out.println("jan");
 		
 	}
 
